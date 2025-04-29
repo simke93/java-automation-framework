e44620a85c220703142725076fbe5adb7db8406f
Ivan <simke93@gmail.com>
2025-04-29 13:31:37 +0400

    Add controller class

--- src/test/java/apiTest/PetUserTests.java
+++ src/test/java/apiTest/PetUserTests.java
@@ -1,5 +1,8 @@
 package apiTest;
 
+import controller.UserController;
+import dto.User;
+import dto.UserResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -39,6 +42,19 @@ public class PetUserTests {
     }
 
     @Test
+    void createUserImprovedTest() {
+        User user = new User(0, "string", "string", "string", "string",
+                "string","string", 0);
+
+        UserController userController = new UserController();
+        Response response = userController.createUser(user);
+        response.getBody().prettyPrint();
+
+        Assertions.assertEquals(200, response.statusCode());
+    }
+
+
+    @Test
     void checkUserResponseBody() {
         String baseUrl = "https://petstore.swagger.io/v2/";
         String body = """
@@ -68,5 +84,21 @@ public class PetUserTests {
                 .body("message", notNullValue(String.class));
     }
 
+    @Test
+    void checkUserResponseBodyImprovedTest() {
+        User user = new User(0, "string", "string", "string", "string",
+                "string","string", 0);
+
+        UserController userController = new UserController();
+        Response response = userController.createUser(user);
+        response.getBody().prettyPrint();
+        UserResponse createdUser = response.as(UserResponse.class);
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals(200, createdUser.getCode());
+        Assertions.assertEquals("unknown", createdUser.getType());
+        Assertions.assertNotNull(createdUser.getMessage());
+        Assertions.assertTrue(Long.parseLong(createdUser.getMessage()) > 9223372036854762719L);
+    }
 
 }
