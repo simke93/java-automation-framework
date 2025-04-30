a920c4280a8e1a98e1fc20bffb75625aa37ed974
Ivan <simke93@gmail.com>
2025-04-30 10:47:28 +0400

    Add POM

---
+++ src/test/java/ui/BaseTest.java
@@ -0,0 +1,28 @@
+package ui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    WebDriver driver;
+
+    protected static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
+
+    @BeforeEach
+    void setup(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+    }
+
+}
