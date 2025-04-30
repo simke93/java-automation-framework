70c315f0035d4de73bae3b05e41bc925764d71e4
Ivan <simke93@gmail.com>
2025-04-30 13:44:20 +0400

    add selenium remote

--- src/test/java/ui/BaseTest.java
+++ src/test/java/ui/BaseTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Map;
 
